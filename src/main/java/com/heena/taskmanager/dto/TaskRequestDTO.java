86d3dc9d0cea700e2cdbbc019d8bbf60582b6182
heenabhatia <heenabhatia363@gmail.com>
2026-06-06 14:43:28 +0530

    Add TaskRequestDTO

---
+++ src/main/java/com/heena/taskmanager/dto/TaskRequestDTO.java
@@ -0,0 +1,31 @@
+package com.heena.taskmanager.dto;
+
+public class TaskRequestDTO {
+
+    private String title;
+    private boolean completed;
+
+    public TaskRequestDTO() {
+    }
+
+    public TaskRequestDTO(String title, boolean completed) {
+        this.title = title;
+        this.completed = completed;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public boolean isCompleted() {
+        return completed;
+    }
+
+    public void setCompleted(boolean completed) {
+        this.completed = completed;
+    }
+}
